1b9b47c2f4534328a27dcf3c99d0d936df38732d
bbnsdevelop <bbnsdevelop@gmail.com>
2024-01-18 22:04:33 -0300

    adding css

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import './index.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // const message = "Hello world react"
