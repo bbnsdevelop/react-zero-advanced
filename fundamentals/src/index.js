a932b6ad0cbd58b36186d1ded55a352f099becb0
bbnsdevelop <bbnsdevelop@gmail.com>
2024-01-18 21:58:05 -0300

    creating my first project in react

---
+++ fundamentals/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// const message = "Hello world react"
+const message = 
+  <div>
+    <h1>Hello world react</h1>
+  </div>
+root.render(message, root);
