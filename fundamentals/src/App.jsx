697dfd9c9ea2bd887a715963abfbcfd14e285b97
bbnsdevelop <bbnsdevelop@gmail.com>
2024-01-29 22:56:42 -0300

    Chore: adding flex-box

--- fundamentals/src/App.jsx
+++ fundamentals/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./index.css";
+import "./App.css"
 
 import FirstComponent from "./components/basic/FirstComponent";
 import ComponentWithParam from "./components/basic/ComponentWithParam";
@@ -8,38 +8,41 @@ import FragmentsComponent from "./components/basic/FragmentsComponent";
 import Calc from "./components/basic/CalcNumberRandomBetweenMinAndMax";
 import Card from "./components/layout/Card";
 
-
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (_) => (
-  <div>
+  <div className="App">
+    
     <h1>Hello world react</h1>
-    <Card title="example of card">
-      <Calc min={2} max={50}> </Calc>
-    </Card>
     
-    <Card title="Fragment">
-      <FragmentsComponent></FragmentsComponent> 
-    </Card>
+    <div className="Cards">
+      <Card title="example of card">
+        <Calc min={2} max={50}>
+          {" "}
+        </Calc>
+      </Card>
 
-    <Card title="Components with params">
-      <ComponentWithParam
-        title="School results"
-        student="Pedro"
-        note={9.5}
-      ></ComponentWithParam>
-      
-      <ComponentWithParam
-        title="School results"
-        student="Jhon"
-        note={5}
-      ></ComponentWithParam>
-    </Card>
+      <Card title="Fragment">
+        <FragmentsComponent></FragmentsComponent>
+      </Card>
+
+      <Card title="Components with params">
+        <ComponentWithParam
+          title="School results"
+          student="Pedro"
+          note={9.5}
+        ></ComponentWithParam>
+
+        <ComponentWithParam
+          title="School results"
+          student="Jhon"
+          note={5}
+        ></ComponentWithParam>
+      </Card>
+
+      <Card title="Fisrt component">
+        <FirstComponent></FirstComponent>
+      </Card>
+    </div>
     
-    <Card title="Fisrt component">
-      <FirstComponent></FirstComponent>
-    </Card>
-
-    
-
   </div>
 );
