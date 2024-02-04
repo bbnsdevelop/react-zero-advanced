45f4531ee7e7babdff5e0b29092434382dfe7136
bbnsdevelop <bbnsdevelop@gmail.com>
2024-02-03 21:01:22 -0300

    chore: comments about spread operator

--- fundamentals/src/components/basic/component-structure/ParentByPropsChildren.jsx
+++ fundamentals/src/components/basic/component-structure/ParentByPropsChildren.jsx
@@ -5,6 +5,11 @@ import React from "react";
 const ParentByCloneOneElementPropsChildren = (props) =>{
     return (
         <div>
+            {/* 
+                {...props} this is spread operator javascript
+                will pass all properties contained in props for the child
+            
+            */}
             {React.cloneElement(props.children, {...props})}            
         </div>
     );
